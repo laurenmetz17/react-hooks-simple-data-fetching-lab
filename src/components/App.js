0a1b3de28eb9e9e003f1c7a07525c79d3d4300e3
Lauren Metz <Laurenmetz15@gmail.com>
2022-10-13 22:54:47 -0400

    commit tree 822a0b9305790a3897d25df93361329fc51d03ef

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,21 @@
 // create your App component here
+import React, {useState, useEffect} from 'react';
+
+function App() {
+
+    const [dogImg, setDogImg] = useState(null);
+
+    useEffect(() => {
+        fetch("https://dog.ceo/api/breeds/image/random").then(resp => resp.json())
+        .then((dog) => {
+            console.log(dog.message);
+            setDogImg(dog.message)
+        })
+    }, []);
+
+    return (
+        dogImg ? <img alt="A Random Dog" src={dogImg}></img> : <p>Loading...</p> 
+    )
+}
+
+export default App;
